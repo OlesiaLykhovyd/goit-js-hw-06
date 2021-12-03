805d4c271930f3343ab2a47d4e3829532fc74712
OlesiaLykhovyd <olesyalykhovyd@gmail.com>
2021-12-03 12:13:31 +0200

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+// Напиши скрипт который, при наборе текста в инпуте
+// input#name-input (событие input), подставляет его текущее
+// значение в span#name-output. Если инпут пустой, в спане
+// должна отображаться строка "Anonymous".
+
+const inputEl = document.getElementById("name-input");
+const outputEl = document.getElementById("name-output");
+
+inputEl.addEventListener("input", (event) => {
+  outputEl.textContent = event.currentTarget.value || "Anonymous";
+});
